b43df25fe7819e8aa13c9fb0ffa124a2ee7a22f4
ievlks <ieva.lekstutyte@telesoftas.com>
2021-01-04 17:16:41 +0200

    config

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ app.get("/", (req,res)=>{
   res.json({message: "Welcome"})
 });
 app.use("/api", userRouter);
-const port = process.env.PORT || 4000;
-// app.listen(port, () => {
-//   console.log("server up and running on PORT :", port);
-// });
-app.listen(process.env.PORT || 4000, function(){
+
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
